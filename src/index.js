74fd11b412d5cbb3a29ca53db8820740e9fb6d4e
Sourav Chhimpa <souravchhimpa1@gmail.com>
2025-03-06 16:33:06 +0530

    basic form of connection will be created

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose"
+import { DB_NAME } from "./constants"
+import express from 'express'
+
+const app = express()
+
+( async()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME} `);
+        app.on("error", (error) => {
+            console.log(error);
+            throw error;
+        });
+        app.listen(process.env.PORT, () => {
+            console.log(`your app is run on the ${process.env.PORT} PORT`);
+            
+        })
+    } catch (error) {
+        console.error("ERROR: ", error) 
+        throw error;
+    }
+})()
