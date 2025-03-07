fc31e979d8fa6b0db3a0c962e47505c9a5e8ee92
Sourav Chhimpa <souravchhimpa1@gmail.com>
2025-03-07 18:44:13 +0530

    api errors handler class will be created

---
+++ src/utils/ApiError.js
@@ -0,0 +1,23 @@
+class ApiError extends Error {
+    constructor(
+        statusCode,
+        messge = 'Something went wrong',
+        errors = [],
+        statck = ""
+    ){
+        super(messge)
+        this.statusCode = statusCode
+        this.data = null
+        this.message = messge
+        this.success = false
+        this.errors = errors
+
+        if (statck) {
+            this.stack = statck
+        } else {
+            Error.captureStackTrace(this, this.constructor)
+        }
+    }
+}
+
+export { ApiError }
