3c1c32061eb55f287b5d49bf6f485e261c3165ab
Sourav Chhimpa <souravchhimpa1@gmail.com>
2025-03-07 17:11:29 +0530

    asynchander will be added to use the express by default

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -5,3 +5,20 @@ const asyncHandler = (requestHandler) => {
 }
 
 export {asyncHandler}
+
+/*
+This is the another way with try catch 
+
+const asyncHandlers = (requestHandler) => async (req, res, next) => {
+    try {
+        await requestHandler(req, res, next)
+    } catch (error) {
+        res.status(err.code || 500).json({
+            sucess: false,
+            message: err.message 
+        })
+    }
+}
+
+export {asyncHandlers}
+*/
