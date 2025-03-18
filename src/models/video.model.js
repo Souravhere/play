a862bf2941a3bef93b0cfbec01d0cc4c8b7ecc6e
Sourav Chhimpa <souravchhimpa1@gmail.com>
2025-03-18 16:49:48 +0530

    paginate will be added on the video model

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,4 +1,5 @@
 import mongoose, {Schema} from "mongoose";
+import paginate from 'mongoose-paginate-v2';
 
 const videoSchema = new Schema(
     {
@@ -37,4 +38,5 @@ const videoSchema = new Schema(
     },
     {timestamps:true});
 
+videoSchema.plugin(paginate)
 export const Video = mongoose.model("Video", videoSchema);
