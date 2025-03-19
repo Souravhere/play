cc90418a6f2a418a4e4aeb84907ecf014dd44944
Sourav Chhimpa <souravchhimpa1@gmail.com>
2025-03-19 16:46:29 +0530

    cloudinary basic config will be added

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,2 +1,9 @@
 import {v2 as cloudinary} from 'cloudinary'
 import fs from fs;
+
+    // Configuration
+    cloudinary.config({ 
+        cloud_name: 'dwektnxhs', 
+        api_key: '318689449591635', 
+        api_secret: '<your_api_secret>'
+    });
