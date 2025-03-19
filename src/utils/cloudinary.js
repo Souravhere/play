da7018ff3f8d60766227c42786e93aa2a80bb90c
Sourav Chhimpa <souravchhimpa1@gmail.com>
2025-03-19 16:52:27 +0530

    cloudinary config wil be linked

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -3,7 +3,7 @@ import fs from fs;
 
     // Configuration
     cloudinary.config({ 
-        cloud_name: 'dwektnxhs', 
-        api_key: '318689449591635', 
-        api_secret: '<your_api_secret>'
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+        api_key: process.env.CLOUDINARY_API_KEY, 
+        api_secret: process.env.CLOUDINARY_API_SECRET
     });
