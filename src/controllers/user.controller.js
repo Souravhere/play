003a9266d4e790e745c058715c3bc7a5b41c2fe8
Sourav Chhimpa <souravchhimpa1@gmail.com>
2025-03-21 18:25:50 +0530

    now existed user validity check will be added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
-import { ApiError } from "../utils/ApiError.js"
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.model.js"
 
 const registerUser = asyncHandler( async (req, res) =>{
     // res.status(200).json({
@@ -13,6 +14,15 @@ const registerUser = asyncHandler( async (req, res) =>{
         [fullName, email, userName, password].some((field) => field?.trim() === "")
     ) { 
         throw new ApiError(400, "All fileds are required")
+    }
+
+    // here we check the user will not exist first time 
+    const existedUser = User.findOne({
+        $or: [{userName},{email}]
+    })
+
+    if (existedUser) {
+        throw new ApiError(409, "User with email and userName all ready exist!")
     }
 });
 
