1792f9e9e20df33d72ad4d6fbf3d4c8bb40950b1
Sourav Chhimpa <souravchhimpa1@gmail.com>
2025-03-21 18:47:13 +0530

    media file handlers will be added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -24,6 +24,14 @@ const registerUser = asyncHandler( async (req, res) =>{
     if (existedUser) {
         throw new ApiError(409, "User with email and userName all ready exist!")
     }
+
+    // here we handled the Avatar and coverImage
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    if (!avatarLocalPath) {
+        throw new ApiError(400, "Avatar file is required!")
+    }
 });
 
 export {registerUser}
