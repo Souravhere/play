6076363d76fa255c78fe1203118704b830595814
Sourav Chhimpa <souravchhimpa1@gmail.com>
2025-03-21 18:14:48 +0530

    a basic field check will be added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,13 +1,19 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js"
 
 const registerUser = asyncHandler( async (req, res) =>{
     // res.status(200).json({
     //     message: "ok"
     // });
 
-    const {fullName, email, userName} = req.body;
+    const {fullName, email, userName, password} = req.body;
     console.log("email :", email);
-    
+    // here we will check that all fields have data they not empty
+    if (
+        [fullName, email, userName, password].some((field) => field?.trim() === "")
+    ) { 
+        throw new ApiError(400, "All fileds are required")
+    }
 });
 
 export {registerUser}
