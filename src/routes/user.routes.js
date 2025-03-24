5ad975c017b9477f260c04471e47003a8c59d2bd
Sourav Chhimpa <souravchhimpa1@gmail.com>
2025-03-24 18:57:17 +0530

    refreshAccessToken route will be created in the route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser, logoutUser, loginUser } from "../controllers/user.controller.js";
+import { registerUser, logoutUser, loginUser, refreshAccessToken } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middlewares.js";
 import  {verifyJWT}  from "../middlewares/auth.middleware.js"
 
@@ -24,5 +24,6 @@ router.route("/login").post(loginUser)
 
 // protected routes
 router.route("/logout").post(verifyJWT, logoutUser)
+router.route("/refresh-token").post(refreshAccessToken)
 
 export default router;
