37124c58fa8aeb562377ccbdf75d9f9675b867dd
Sourav Chhimpa <souravchhimpa1@gmail.com>
2025-03-28 17:30:00 +0530

    Add new user routes for password change, current user retrieval, account updates, avatar and cover image changes, and watch history

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser, logoutUser, loginUser, refreshAccessToken } from "../controllers/user.controller.js";
+import { registerUser, logoutUser, loginUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, coverImageUpdate, getUserChannelProfile, getWatchHistory } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middlewares.js";
 import  {verifyJWT}  from "../middlewares/auth.middleware.js"
 
@@ -25,5 +25,16 @@ router.route("/login").post(loginUser)
 // protected routes
 router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT,changeCurrentPassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-account").patch(verifyJWT,updateAccountDetails) // patch will be used when we don't update the complete data just update few fiels
+
+// handled the file 
+router.route("/change-avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/change-coverImage").patch(verifyJWT, upload.single("coverImage"), coverImageUpdate)
+router.route("/history").get(verifyJWT, getWatchHistory)
+
+// here we will get the data from the pramps "url"
+router.route("/channel/:userName").get(verifyJWT,getUserChannelProfile)
 
 export default router;
